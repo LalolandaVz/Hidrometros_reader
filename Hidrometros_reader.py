d9e2e32e61dbeee5eeb821de8dcfa8a6e9ca6686
Sergio Vz <sergio.veliz.92@gmail.com>
2022-01-19 08:21:01 -0300

    Update
    Update, better written, new Info_hidrometros.txt and Folders_datos.txt files, more hydrometers.

--- Hidrometros_reader.py
+++ Hidrometros_reader.py
@@ -4,46 +4,162 @@ Created on Sun Jan  2 02:54:05 2022
 
 @author: hid28serv
 """
-
-import pytesseract as pytesseract
-from PIL import Image
-from selenium import webdriver
-import numpy as np
 from datetime import datetime
+from datetime import timedelta
+import pytesseract
+from PIL import Image
+import numpy as np
 import time
 import schedule
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR/tesseract.exe'
-TESSDATA_PREFIX = 'C:\Program Files\Tesseract-OCR'
-tessdata_dir_config = '--tessdata-dir "C:\Program Files\Tesseract-OCR\tesseract"'
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+TESSDATA_PREFIX = 'C:/Program Files/Tesseract-OCR'
+tessdata_dir_config = '--tessdata-dir "C:/Program Files/Tesseract-OCR/tesseract"'
 
 
 def get_text(image):
     return pytesseract.image_to_string(image)
 
+#file_destination = IMAGES_FOLDER + Name_URL[i] + '.png'
+def screenshot_cut_read_1(URL,file_destination):
 
-URL_Ramallo = 'http://hidrografia2.agpse.gob.ar/Ramallo/index.html'
-URL_SanNicolas = 'http://hidrografia2.agpse.gob.ar/SanNicolas/index.html'
-URL_VillaConstitucion = 'http://hidrografia2.agpse.gob.ar/VillaConstitucion/index.html'
-URL_Rosario = 'http://hidrografia2.agpse.gob.ar/Rosario/index.html'
-URL_SanLorenzo = 'http://hidrografia2.agpse.gob.ar/SanLorenzo/index.html'
-URL_Parana = 'http://hidrografia2.agpse.gob.ar/Parana/index.html'
-URL_SantaFe = 'http://hidrografia.agpse.gob.ar:53880/SantaFe/index.html'
-URL_Hernandarias = 'http://hidrografia2.agpse.gob.ar/Hernandarias/index.html'
-URL_SantaElena = 'http://hidrografia.agpse.gob.ar:53880/SantaElena/index.html'
-URL_LaPaz = 'http://hidrografia2.agpse.gob.ar/LaPaz/index.html'
-URL_Reconquista = 'http://hidrografia2.agpse.gob.ar/Reconquista/index.html'
-URL_Goya = 'http://hidrografia2.agpse.gob.ar/Goya/index.html'
-URL_BellaVista='http://hidrografia2.agpse.gob.ar/BellaVista/index.html'
-URL_Empedrado = 'http://hidrografia2.agpse.gob.ar/Empedrado/index.html'
-URL_Barranqueras = 'http://hidrografia2.agpse.gob.ar/Barranqueras/index.html'
-URL_Corrientes='http://hidrografia2.agpse.gob.ar/Corrientes/index.html'
-URL_PasodelaPatria='http://hidrografia2.agpse.gob.ar/PasodelaPatria/index.html'
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(r"chromedriver.exe",chrome_options=options)
+    driver.get(URL)
+    driver.execute_script("document.body.style.zoom='250%'")
+    #get window size
+    s = driver.get_window_size()
+    #obtain browser height and width
+    w = driver.execute_script('return document.body.parentNode.scrollWidth')
+    h = driver.execute_script('return document.body.parentNode.scrollHeight')
+    #set to new window size
+    driver.set_window_size(w*25, h*25)
 
-BASE_URL = [URL_Ramallo,URL_SanNicolas,URL_VillaConstitucion,URL_Rosario,URL_SanLorenzo,URL_Parana,URL_SantaFe,URL_Hernandarias,URL_SantaElena,URL_LaPaz,URL_Reconquista,URL_Goya,URL_BellaVista,URL_Empedrado,URL_Barranqueras,URL_Corrientes,URL_PasodelaPatria]
-Name_URL = ['Ramallo','San Nicolas','Villa Constitucion','Rosario','San Lorenzo','Parana','Santa Fe','Hernandarias','Santa Elena','La Paz','Reconquista','Goya','Bella Vista','Empedrado','Barranqueras','Corrientes','Paso de La Patria']
-IMAGES_FOLDER = 'C:/Users/Marea/Documents/Hidrometros_reader/IMAGES_files/'
-DATA_FOLDER = 'C:/Users/Marea/Documents/Hidrometros_reader/DATA_files/'
+    #Tardan mas en abrir estos hidrometros, le doy tiempo:
+    try:
+        WebDriverWait(driver, 6).until(EC.presence_of_element_located((
+            By.CLASS_NAME, 'clips-cards ')))
+    except TimeoutException:
+        print('Page timed out after 6 secs.')
+
+    #obtain screenshot of page within body tag
+    driver.find_element_by_tag_name('body').screenshot(file_destination)
+    driver.set_window_size(s['width'], s['height'])
+    driver.quit()
+
+    img = Image.open(file_destination)
+
+    img_cut= img.crop((750,180,925,350))
+    Tide = get_text(img_cut)
+
+    img_cut= img.crop((1275,180,1605,350))
+    Date = get_text(img_cut)
+
+    img_cut= img.crop((1600,180,1865,350))
+    Times = get_text(img_cut)
+
+    return [Tide,Date,Times]
+
+def screenshot_cut_read_2(URL,file_destination):
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(r"chromedriver.exe",chrome_options=options)
+    driver.get(URL)
+    driver.execute_script("document.body.style.zoom='250%'")
+    #get window size
+    s = driver.get_window_size()
+    #obtain browser height and width
+    w = driver.execute_script('return document.body.parentNode.scrollWidth')
+    h = driver.execute_script('return document.body.parentNode.scrollHeight')
+    #set to new window size
+    driver.set_window_size(w*25, h*25)
+
+    try:
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((
+            By.CLASS_NAME, 'clips-cards ')))
+    except TimeoutException:
+        print('Page timed out after 2 secs.')
+
+    #obtain screenshot of page within body tag
+    driver.find_element_by_tag_name('body').screenshot(file_destination)
+    driver.set_window_size(s['width'], s['height'])
+    driver.quit()
+
+    img = Image.open(file_destination)
+
+    img_cut= img.crop((680,180,810,290))
+    Tide = get_text(img_cut)
+
+    img_cut= img.crop((1055,180,1353,290))
+    Date = get_text(img_cut)
+
+    img_cut= img.crop((1360,180,1600,290))
+    Times = get_text(img_cut)
+
+    return [Tide,Date,Times]
+
+def screenshot_cut_read_3(URL,file_destination):
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(r"chromedriver.exe",chrome_options=options)
+    driver.get(URL)
+    driver.execute_script("document.body.style.zoom='500%'")
+    #get window size
+    s = driver.get_window_size()
+    #obtain browser height and width
+    w = driver.execute_script('return document.body.parentNode.scrollWidth')
+    h = driver.execute_script('return document.body.parentNode.scrollHeight')
+    #set to new window size
+    driver.set_window_size(w*50, h*50)
+
+    try:
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((
+            By.CLASS_NAME, 'clips-cards ')))
+    except TimeoutException:
+        print('Page timed out after 2 secs.')
+
+    #obtain screenshot of page within body tag
+    driver.find_element_by_tag_name('body').screenshot(file_destination)
+    driver.set_window_size(s['width'], s['height'])
+    driver.quit()
+
+    img = Image.open(file_destination)
+
+    img_cut= img.crop((1380,410,1600,580))
+    Tide = get_text(img_cut)
+
+    img_cut= img.crop((2150,410,2700,580))
+    Date = get_text(img_cut)
+
+    img_cut= img.crop((2730,410,3200,580))
+    Times = get_text(img_cut)
+
+    return [Tide,Date,Times]
+
+
+Info_hidrometros = np.genfromtxt(r'Info_hidrometros.txt',delimiter='	',dtype=str)
+folders = np.genfromtxt(r'Folders_datos.txt',delimiter='	',dtype=str)
+
+#index es el indice con el que comienza la string de cada hidrometro.
+
+index = Info_hidrometros[:,0]
+index = list(index)
+
+for i in range(0,len(Info_hidrometros[:,0])):
+    index[i] = int(index[i])
+
+BASE_URL = Info_hidrometros[:,2]
+Name_URL = Info_hidrometros[:,1]
+IMAGES_FOLDER = folders[0]
+DATA_FOLDER = folders[1]
 
 
 
@@ -52,124 +168,140 @@ def job():
     Times = ['']*len(BASE_URL)
     Date = ['']*len(BASE_URL)
     Emptys=['']*len(BASE_URL)
-    for h in range(0,len(Times)): Emptys[h]=Times[h]==''
+    print('comienza ciclo')
+    print(datetime.now())
 
-    while any(Emptys):    
+    for h in range(0,len(Times)):
+        Emptys[h]=Times[h]==''
+    repeater = 0 #indice para limitar las repeticiones cuando no encuentra fecha en el sitio
+    while any(Emptys):
+        repeater = repeater + 1
+        print('repeater =' + str(repeater))
+        if repeater==4:
+            print('repeater =' + str(repeater) + ' Corta ciclo')
+            break
+
         for i in range(0,len(BASE_URL)):
 
-            if Times[i]=='':            
-                #BASE_URL = 'http://hidrografia2.agpse.gob.ar/SanLorenzo/index.html'
-                print(datetime.now())
-    
-                
-                options = webdriver.ChromeOptions()
-                options.add_argument('headless')
-                
-                
-                driver = webdriver.Chrome(r"chromedriver.exe",chrome_options=options)
-                
-                driver.get(BASE_URL[i])
-                driver.execute_script("document.body.style.zoom='250%'")
-                #get window size
-                s = driver.get_window_size()
-                #obtain browser height and width
-                w = driver.execute_script('return document.body.parentNode.scrollWidth')
-                h = driver.execute_script('return document.body.parentNode.scrollHeight')
-                #set to new window size
-                driver.set_window_size(w*25, h*25)
-                
-                
-                
-                #obtain screenshot of page within body tag
-                driver.find_element_by_tag_name('body').screenshot(IMAGES_FOLDER + Name_URL[i] + '.png')
-                driver.set_window_size(s['width'], s['height'])
-                driver.quit()
-                
-                img = Image.open(IMAGES_FOLDER + Name_URL[i] + '.png')
-                
-                img_cut= img.crop((680,180,810,290))
-                Tide[i] = get_text(img_cut)
-                
-                
-                img = Image.open(IMAGES_FOLDER + Name_URL[i] + '.png')
-                
-                img_cut= img.crop((1055,180,1353,290))
-                Date[i] = get_text(img_cut)
-                
-                
-                
-                img = Image.open(IMAGES_FOLDER + Name_URL[i] + '.png')
-                
-                img_cut= img.crop((1360,180,1600,290))
-                Times[i] = get_text(img_cut)
+            if Times[i] == '':
+                print('Hidrometro '+ Name_URL[i])
+                #Si estoy viendo Brasileria u Oyarvide, tiene distinto tamaño la imagen:
+                if Name_URL[i] == 'Brasileira' or Name_URL[i] == 'Oyarvide':
 
-        for h in range(0,len(Times)): Emptys[h]=Times[h]==''        
-    
-    
-    
-    for i in range(len(Tide)):
+                    [Tide[i],Date[i],Times[i]] = screenshot_cut_read_1(
+                        BASE_URL[i],IMAGES_FOLDER + Name_URL[i] + '.png')
+
+                    print('Tide = ' + Tide[i])
+
+                elif Name_URL[i] == 'Carabelitas': #si estoy viendo cualquiera que no sea brasileira y oyarvide
+
+                    [Tide[i],Date[i],Times[i]] = screenshot_cut_read_3(
+                        BASE_URL[i],IMAGES_FOLDER + Name_URL[i] + '.png')
+
+                    print('Tide = ' + Tide[i])
+
+                else:
+                    
+                    [Tide[i],Date[i],Times[i]] = screenshot_cut_read_2(
+                        BASE_URL[i],IMAGES_FOLDER + Name_URL[i] + '.png')
+
+                    print('Tide = ' + Tide[i])
+
+        for h in range(0,len(Times)):
+            Emptys[h]=Times[h]==''
+
+        print('¿Any emptys?')
+        print(Emptys)
+        print(any(Emptys))
+
+    #print('todavia sigue 0')
+    for i in range(0,len(Times)):
+        if Times[i]=='': #para el caso en que falle la web
+            Times[i] = '   '
+            Date[i] = '   '
+
         Tide[i]=Tide[i][0:-1]
         Times[i]=Times[i][0:-1]
         Date[i]=Date[i][0:-1]
 
-    for i in range(0,len(Tide)):
-        if Tide[i]=='NAN':
+        if Tide[i]=='NAN' or Tide[i]=='':
             Tide[i]='-9999.0'
-        elif Tide[i]=='':
-            Tide[i]='-9999.0'
-    
+
+    #print('todavia sigue 2')
     DateandTime = ['None']*len(Date)
     for i in range(0,len(Times)):
         DateandTime[i] = Date[i] + ' ' + Times[i]
-        DateandTime[i] = datetime.strptime(DateandTime[i],'%d/%m/%Y %H:%M:%S')
-        DateandTime[i] = DateandTime[i].strftime('%Y-%m-%d %H:%M:%S')
-        
-    
-    
-    index=[95,29,30,27,99,19,2,5,6,7,10, 9,11,12,16,13,4]
-    index2=12   
+        if Times[i]=='   ' or Date[i]=='   ':
+            DateandTime[i]=''
+        else:
+            if Name_URL[i] == 'Brasileira' or Name_URL[i] == 'Oyarvide':
+                #esta escrita distinta la fecha para oyarvide y brasileira
+                print('')
+            else:
+                DateandTime[i] = datetime.strptime(DateandTime[i],'%d/%m/%Y %H:%M:%S')
+                DateandTime[i] = DateandTime[i].strftime('%Y-%m-%d %H:%M:%S')
+
+    #print('todavia sigue 3')
+
+    index2=12   #otro index que deberia tener un numero segun hidrometro,
+    #pero al tidemanager no le molesta su valor
     for i in range(0,len(Times)):
-        
-        
+
+
         try:
             HIDROMETRO = np.genfromtxt(DATA_FOLDER + Name_URL[i] + '.txt',delimiter='\n',dtype=str)
             HIDROMETRO = list(HIDROMETRO)
-            string=str(index[i])+'|Estacion ' + Name_URL[i] + '|' + DateandTime[i] +'|'+str(index2)+'|Level|'+Tide[i]+'|NULL'
-            if string!=HIDROMETRO[-1]:
-                HIDROMETRO.append(string)
-                HIDROMETRO = np.array(HIDROMETRO)
-                np.savetxt(DATA_FOLDER + Name_URL[i] + '.txt',HIDROMETRO,fmt='%s')            
-            
+            #print('todavia sigue 4')
+            if DateandTime[i]=='':
+                print('no se agrega')
+            else:
+                #print('todavia sigue 5, i=' + str(i))
+                if Name_URL[i] == 'Brasileira' or Name_URL[i] == 'Baliza Mitre':
+                    string=str(index[i])+ '|' + Name_URL[i] + '|' + \
+                        DateandTime[i] +'|'+str(index2)+'|Level|'+Tide[i]+'|NULL'
+                else:
+                    string=str(index[i])+'|Estacion ' + Name_URL[i] + \
+                        '|' + DateandTime[i] +'|'+str(index2)+'|Level|'+Tide[i]+'|NULL'
+
+                if string!=HIDROMETRO[-1]:
+                    HIDROMETRO.append(string)
+                    HIDROMETRO = np.array(HIDROMETRO)
+                    np.savetxt(DATA_FOLDER + Name_URL[i] + '.txt',HIDROMETRO,fmt='%s')
+
         except:
-            HIDROMETRO = list([str(index[i])+'|Estacion ' + Name_URL[i] + '|' + DateandTime[i] +'|'+str(index2)+'|Level|'+Tide[i]+'|NULL'])
-            HIDROMETRO.append(HIDROMETRO[0])
-            HIDROMETRO = np.array(HIDROMETRO)
-            np.savetxt(DATA_FOLDER + Name_URL[i] + '.txt',HIDROMETRO,fmt='%s')
-    
-        index2 = index2 + 1 
+            #print('todavia sigue 4')
+            if DateandTime[i]=='':
+                print('no se agrega')
+            else:
 
+                if Name_URL[i] == 'Brasileira' or Name_URL[i] == 'Baliza Mitre':
+                    HIDROMETRO = list([str(index[i])+ '|' + Name_URL[i] + '|' + \
+                                       DateandTime[i] +'|'+str(index2)+'|Level|'+Tide[i]+'|NULL'])
+                else:
+                    HIDROMETRO = list([str(index[i])+'|Estacion ' + Name_URL[i] + '|' + \
+                                       DateandTime[i] +'|'+str(index2)+'|Level|'+Tide[i]+'|NULL'])
+                HIDROMETRO.append(HIDROMETRO[0])
+                HIDROMETRO = np.array(HIDROMETRO)
+                np.savetxt(DATA_FOLDER + Name_URL[i] + '.txt',HIDROMETRO,fmt='%s')
 
-Actualizacion = ["00:02","02:00","04:00","06:02","08:00","10:00","12:02","14:00","16:00","18:02","20:00","22:00"]
-schedule.every().day.at(Actualizacion[0]).do(job)
-schedule.every().day.at(Actualizacion[1]).do(job)
-schedule.every().day.at(Actualizacion[2]).do(job)
-schedule.every().day.at(Actualizacion[3]).do(job)
-schedule.every().day.at(Actualizacion[4]).do(job)
-schedule.every().day.at(Actualizacion[5]).do(job)
-schedule.every().day.at(Actualizacion[6]).do(job)
-schedule.every().day.at(Actualizacion[7]).do(job)
-schedule.every().day.at(Actualizacion[8]).do(job)
-schedule.every().day.at(Actualizacion[9]).do(job)
-schedule.every().day.at(Actualizacion[10]).do(job)
-schedule.every().day.at(Actualizacion[11]).do(job)
+        index2 = index2 + 1
+
+Actualizacion = []
+horario = datetime(2022,1,1,0,0,0)
+Actualizacion.append(horario.strftime('%H:%M'))
+for i in range(0,287):
+    horario = horario + timedelta(minutes=5)
+    Actualizacion.append(horario.strftime('%H:%M'))
+
+for i in range(0,len(Actualizacion)):
+    schedule.every().day.at(Actualizacion[i]).do(job)
+
+#schedule.every().day.at('17:57').do(job)
 
 while 1:
     try:
-        schedule.run_pending()    
+        schedule.run_pending()
         time.sleep(1)
     except:
 
         continue
-
-
-
